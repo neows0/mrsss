59dd3df909fe2ad131ede26b5e0777c0229bfc86
alanxoc3 <alanxoc3@gmail.com>
2017-06-24 16:50:12 -0600

    mr sss is coming

---
+++ desktop/src/com/ja/mrsss/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ja.mrsss.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ja.mrsss.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
